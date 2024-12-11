56b64cf8e8b8766d501a22333fd43576ad6ae5a1
venyo wong <venyowong@163.com>
2024-12-11 22:34:00 +0800

    完善

--- V.WebDav/WebDavService.cs
+++ V.WebDav/WebDavService.cs
@@ -25,15 +25,116 @@ namespace V.WebDav
             return reader.ReadToEnd();
         }
 
-        public void Dispose()
-        {
-            this.client.Dispose();
-        }
-
         public async Task<T> Get<T>(string path)
         {
             var result = await this.Get(path);
             return result.ToObj<T>();
+        }
+
+        public async Task<List<NoteItem>> GetNotes(string path)
+        {
+            var note = await this.Get(path);
+            if (string.IsNullOrEmpty(note))
+            {
+                return null;
+            }
+
+            try
+            {
+                var list = note.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+                var result = new List<NoteItem>();
+                var parent = new List<NoteItem>();
+                foreach (var item in list)
+                {
+                    if (string.IsNullOrEmpty(item))
+                    {
+                        continue;
+                    }
+
+                    var text = item.TrimStart();
+                    var layers = (item.Length - text.Length) / 2;
+                    if (text.StartsWith('-'))
+                    {
+                        var noteItem = new NoteItem
+                        {
+                            Title = text[1..].Trim(),
+                            Children = []
+                        };
+                        if (layers > parent.Count - 1)
+                        {
+                            if (layers == 0)
+                            {
+                                result.Add(noteItem);
+                                parent.Add(noteItem);
+                            }
+                            else
+                            {
+                                parent[layers - 1].Children.Add(noteItem);
+                                parent.Add(noteItem);
+                            }
+                        }
+                        else if (layers == parent.Count - 1)
+                        {
+                            if (parent.Count == 1)
+                            {
+                                result.Add(noteItem);
+                                parent[0] = noteItem;
+                            }
+                            else
+                            {
+                                parent[layers - 1].Children.Add(noteItem);
+                                parent[layers] = noteItem;
+                            }
+                        }
+                        else
+                        {
+                            if (layers == 0)
+                            {
+                                result.Add(noteItem);
+                                parent[0] = noteItem;
+                            }
+                            else
+                            {
+                                parent[layers - 1].Children.Add(noteItem);
+                                parent[layers] = noteItem;
+                            }
+
+                            for (int i = parent.Count - 1; i > layers; i--)
+                            {
+                                parent.RemoveAt(i);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        layers--;
+                        if (layers < 0 || layers >= parent.Count)
+                        {
+                            continue;
+                        }
+
+                        var p = parent[layers];
+                        if (string.IsNullOrEmpty(p.Description))
+                        {
+                            p.Description = text;
+                        }
+                        else
+                        {
+                            p.Description = $"{p.Description}\n{text}";
+                        }
+                    }
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"{path} 格式不规范", e);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.client.Dispose();
         }
     }
 }
